7550d466c4fc0361561dad76f4a073c2a66366cb
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-09-20 22:56:15 -0300

    v 0.41 chore

--- src/repositories/transcriptionRepositories.ts
+++ src/repositories/transcriptionRepositories.ts
@@ -1,31 +1,42 @@
 import { prisma } from "../lib/prisma"
 
 async function findVideo(videoId: string) {
-    const video = await prisma.video.findUniqueOrThrow({
-        where: {
-            id: videoId,
-        }
-    })
-    return video;
+  const video = await prisma.video.findUniqueOrThrow({
+    where: {
+      id: videoId,
+    }
+  })
+  return video;
 }
 
 
 async function updateVideo(videoId: string, transcription: string, videoName: string, userId: string) {
+  async function updateVideo(videoId: string, transcription: string, videoName: string, userId: string) {
+    const video = await prisma.video.findUnique({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video) {
+      throw new Error(`Video with ID ${videoId} not found.`);
+    }
+    
     await prisma.video.update({
-        where: {
-          id: videoId,
-        },
-        data: {
-          transcription,
-          name: videoName,
-          user: {
-            connect: {
-              id: userId,
-            },
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription,
+        name: videoName,
+        user: {
+          connect: {
+            id: userId,
           },
         },
-      });
-
+      },
+    });
+  }
 }
 
 
